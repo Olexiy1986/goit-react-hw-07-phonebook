7c03ffd275563923b8be7a2cacece14508173d3b
Oleksiy Shovkun <ssapfir1986@ukr.net>
2020-08-01 13:34:56 +0300

    Updates

---
+++ precache-manifest.edc30b43a96a385cda98efe0084f080e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12554997862a7c48f67997ca58e0922",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "f7f15df15851cf9ba508",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.1fd3cbf6.chunk.css"
+  },
+  {
+    "revision": "750744dda1f13448e4ea",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.e8be057e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.e8be057e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f15df15851cf9ba508",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.ed508079.chunk.js"
+  },
+  {
+    "revision": "b0ee86f873d795c0676a",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.8a222599.js"
+  }
+]);
